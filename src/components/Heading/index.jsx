5092c86b75262313f09dc77fdf506934565e1f8f
Richard Yarwood <richyarwood@gmail.com>
2020-06-02 22:50:42 +0100

    additional heading styles

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const headingSizes = {
   h1: 4,
   h2: 2.5,
-  h3: 1.5,
-  h4: 1,
+  h3: 2,
+  h4: 1.5,
 };
 
+const horizontalLine = css`
+  content: '';
+    background-color: var(--jsxBlue);
+    bottom: -6px;
+    height: 2px;
+    left: 0;
+    position: absolute;
+    width: 100%;
+`;
+
 const HeadingStyles = styled.h1`
+  color: ${({ color }) => color && color};
+  display: inline-block;
   font-size: ${headingSizes.h1}rem;
   font-size: ${({ as }) => as && `${headingSizes[as]}rem`};
   font-family: var(--condensedFont);
-  color: ${({ color }) => color && color};
+  line-height: initial;
   margin: 0;
+  position: relative;
   text-align: ${({ center }) => center && 'center'};
   ${(props) => props.moreStyles && props.moreStyles};
+
+  & :after {
+    ${({ underlined }) => underlined && horizontalLine};
+  }
 `;
 
 const Heading = ({
@@ -24,12 +41,14 @@ const Heading = ({
   children,
   color,
   moreStyles,
+  underlined,
 }) => (
   <HeadingStyles
     as={as}
     color={color}
     center={center}
     moreStyles={moreStyles}
+    underlined={underlined}
   >
     {children}
   </HeadingStyles>
