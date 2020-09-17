aa7837ed4ad299fde8434fda2cb66dc2921ac34f
Richard Yarwood <richyarwood@gmail.com>
2020-09-17 22:47:37 +0100

    add footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import { AiFillGithub } from 'react-icons/ai';
+import { IconContext } from 'react-icons';
+
+const FooterStyles = styled.footer`
+  height: 150px;
+  color: var(--white);
+  font-size: var(--md);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-top: 1px solid var(--lightGrey);
+  margin: var(--md);
+`;
+
+const FooterText = styled.p`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 5px 0;
+  margin: 0;
+
+  & > span {
+    margin-left: var(--xxs);
+  }
+`;
+
+const Footer = () => (
+  <IconContext.Provider value={{ color: 'var(--white)', size: '20px' }}>
+    <FooterStyles>
+      <FooterText>
+        This site is created with Gatsby and Styled Components
+      </FooterText>
+      <a
+        href="https://github.com/richyarwood/portfolio"
+        title="Richard Yarwood Full Stack Javascript Developer - GitHub"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <FooterText>
+          <AiFillGithub />
+          <span>Source Repo</span>
+        </FooterText>
+      </a>
+    </FooterStyles>
+  </IconContext.Provider>
+);
+
+export default Footer;
