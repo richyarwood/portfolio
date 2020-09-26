faa6b595b1159796c24707ff3c0113f92c7f91e1
Richard Yarwood <richyarwood@gmail.com>
2020-09-26 21:15:39 +0100

    add orientation in media query

--- src/utils/presets.js
+++ src/utils/presets.js
@@ -3,7 +3,7 @@ module.exports = {
     mobile: '@media (min-width: 400px)',
     phablet: '@media (min-width: 550px)',
     tablet: '@media (min-width: 750px)',
-    desktop: '@media (min-width: 1000px)',
+    desktop: '@media (min-width: 1000px) and (orientation: landscape)',
     xl: '@media (min-width: 1200px)',
     xxl: '@media (min-width: 1600px)',
   },
