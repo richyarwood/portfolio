adcb1d4a7f435a374252f9b64329ddce3a8f0ac6
Richard Yarwood <richyarwood@gmail.com>
2020-10-10 23:10:46 +0100

    remove styling on 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,53 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import styled from 'styled-components';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
-
-const linearFill = (direction) => `linear-gradient(${direction}, rgba(255,0,0,1) 0%, rgba(255,54,54,1) 100%)`;
-
-const size = '160px';
-
-const BigRedButtonWrapperStyles = styled.div`
-  background: ${linearFill('to bottom')};
-  border-radius: 50%;
-  cursor: pointer;
-  height: ${size};
-  margin-top: 30px;
-  position: relative;
-  width: ${size};
-
-  &:before {
-    background: ${linearFill('to top')};
-    border-radius: 50%;
-    bottom: 50%;
-    content: '';
-    height: calc(${size} * 0.8);
-    position: absolute;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    width: calc(${size} * 0.8);
-  }
-
-  & p {
-    align-items: center;
-    background-clip: text;
-    -webkit-background-clip: text;
-    -moz-background-clip: text;
-    background-color: #c9c9c9;
-    color: transparent;
-    display: flex;
-    font-size: var(--xxl);
-    font-weight: var(--boldFont);
-    height: 100%;
-    justify-content: center;
-    position: absolute;
-    text-shadow: 0px 2px 3px rgba(255,255,255,0.5);
-    top: 0;
-    width: 100%;
-  }
-`;
 
 const NotFoundPage = () => (
   <Layout>
@@ -61,15 +16,15 @@ const NotFoundPage = () => (
         align-items: center;
       `}
     >
-      <h1>Don&#39;t panic. But this route doesn&#39;t exist!!</h1>
-      <h3>Click the big red button if you dare</h3>
-      <Link
-        to="/"
-      >
-        <BigRedButtonWrapperStyles>
-          <p>GO</p>
-        </BigRedButtonWrapperStyles>
-      </Link>
+      <h1>Uh oh! This page doesn&#39;t exist</h1>
+      <h3>Don&#39;t worry...</h3>
+      <h3>
+        <Link
+          to="/"
+        >
+          Go to the home page
+        </Link>
+      </h3>
     </div>
 
   </Layout>
